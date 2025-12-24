2abf8c08ae7d0882cdb7f8a16c77d32b50666730
raffael-marinho <silvamarinho10@gmail.com>
2025-12-24 15:20:33 -0300

    feat: add mapa de extensoes na config.py

---
+++ src/config.py
@@ -0,0 +1,39 @@
+# Mapeamento de extensões e pastas
+MAPA_EXTENSOES = {
+    # Impressão 3D
+    '.stl': ('Impressão 3D', 'arquivos'),
+    '.obj': ('Impressão 3D', 'arquivos'),
+    '.3mf': ('Impressão 3D', 'arquivos'),
+    '.gcode': ('Impressão 3D', 'arquivos'),
+    # Imagens
+    '.jpg': ('Mídia', 'Fotos'),
+    '.jpeg': ('Mídia', 'Fotos'),
+    '.png': ('Mídia', 'Fotos'),
+    '.heic': ('Mídia', 'Fotos'),
+    '.raw': ('Mídia', 'Fotos'),
+    '.webp': ('Mídia', 'Fotos'),
+    # Vídeos
+    '.mp4': ('Mídia', 'Vídeos'),
+    '.mov': ('Mídia', 'Vídeos'),
+    '.avi': ('Mídia', 'Vídeos'),
+    '.mkv': ('Mídia', 'Vídeos'),
+    # Documentos
+    '.pdf': ('Documentos', 'Mídia/arquivos'),
+    '.docx': ('Documentos', 'Mídia/arquivos'),
+    '.xlsx': ('Documentos', 'Mídia/arquivos'),
+    '.txt': ('Documentos', 'Mídia/arquivos'),
+    '.pptx': ('Documentos', 'Mídia/arquivos'),
+    # Executáveis e Compactados
+    '.exe': ('Software', 'arquivos'),
+    '.msi': ('Software', 'arquivos'),
+    '.iso': ('Software', 'arquivos'),
+    '.zip': ('Software', 'arquivos'),
+    '.rar': ('Software', 'arquivos'),
+    '.7z': ('Software', 'arquivos')
+}
+
+MESES_PT = {
+    1: 'Janeiro', 2: 'Fevereiro', 3: 'Março', 4: 'Abril',
+    5: 'Maio', 6: 'Junho', 7: 'Julho', 8: 'Agosto',
+    9: 'Setembro', 10: 'Outubro', 11: 'Novembro', 12: 'Dezembro'
+}
